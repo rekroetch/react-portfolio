3dc0d6ffb2f5b7148eb7286c5a805658bbf0f4b0
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-09 21:38:10 -0800

    profile pic added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
-
 import './about.css'
+import profPic from '../assets/profilePic.jpeg'
 import Container from '../components/Container'
 import Jumbotron from '../components/Jumbotron'
 import Row from '../components/Row'
@@ -18,7 +18,7 @@ function About() {
                 </Row>   
                 <Row>
                     <Col size="xs-6" className="float-left">
-                        <img src="../../public/assets/profilePic.jpeg" className="card-img float-left" alt="teal shirt"></img>
+                        <img src={profPic} className="cardImg float-left" alt="teal shirt"></img>
                         <p className="float-xs-left">As a full-stack web developer with a Bachelor of Science in Neurobiology from the University of California, Davis, I use my passion for problem solving and excellent time management skills to tackle every task presented to me.  These skills, along with a high level of teamwork and communication, were especially demonstrated during my undergraduate years at UC Davis, working on campus as a kitchen employee before being promoted to Supervisor in less than a year.  Later, while earning a certificate in full stack development from a UC Davis Extension, I refined my technical skills with JavaScript, using it to create dynamic and interesting applications.  I am always eager to expand my knowledge and open to new opportunities in the tech industry where I can bring my customer-first attitude and youthful passion to any team. </p>
                         <p className="float-xs-left">I am currently living in San Jose, CA but open to relocation.  Find me on:</p>
                         <Col size="xs-6" className="float-left">
