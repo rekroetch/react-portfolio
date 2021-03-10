f4a892b327c0e1ef94772c900c4c3eea14477563
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-09 20:36:32 -0800

    container and jumbotron components only

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import Container from '../components/Container'
+import Jumbotron from '../components/Jumbotron'
+
+function Contact() {
+    return (
+        <Container>
+            <Jumbotron>
+                <h1 className="display-4">Contact</h1>
+                <hr className="my-4"></hr>
+            </Jumbotron>
+        </Container>
+    );
+  }
+  
+  export default Contact;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import Container from '../components/Container'
+import Jumbotron from '../components/Jumbotron'
+// import Row from '../components/Row'
+// import Col from '../components/Col'
+
+function Portfolio() {
+    return (
+        <Container>
+            <Jumbotron>
+                <h1 className="display-4">Portfolio</h1>
+                <hr className="my-4"></hr>
+            </Jumbotron>
+        </Container>
+    );
+  }
+  
+  export default Portfolio;
