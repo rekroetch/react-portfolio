6516fc713e90afc346ee52f938232278753cee73
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-09 20:42:25 -0800

    Route to pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import './App.css';
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <div>
+        <Navbar />
+        <div>
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
