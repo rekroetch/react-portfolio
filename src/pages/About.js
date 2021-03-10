3684596f099a1c5e63ed2b4b3a42b383277c8e4b
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-09 20:35:25 -0800

    about page missing image and styling

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+
+import './about.css'
+import Container from '../components/Container'
+import Jumbotron from '../components/Jumbotron'
+import Row from '../components/Row'
+import Col from '../components/Col'
+
+function About() {
+    return (
+        <Container>
+            <Jumbotron>
+                <h1 className="display-4">About Me</h1>
+                <hr className="my-4"></hr>
+                <Row>
+                    <Col size="md-12">
+                        <h4 className="contactInfo">Rachel Kroetch | (408)613-9497 | rkroetch8@gmail.com</h4>
+                    </Col>
+                </Row>   
+                <Row>
+                    <Col size="xs-6" className="float-left">
+                        <img src="../../public/assets/profilePic.jpeg" className="card-img float-left" alt="teal shirt"></img>
+                        <p className="float-xs-left">As a full-stack web developer with a Bachelor of Science in Neurobiology from the University of California, Davis, I use my passion for problem solving and excellent time management skills to tackle every task presented to me.  These skills, along with a high level of teamwork and communication, were especially demonstrated during my undergraduate years at UC Davis, working on campus as a kitchen employee before being promoted to Supervisor in less than a year.  Later, while earning a certificate in full stack development from a UC Davis Extension, I refined my technical skills with JavaScript, using it to create dynamic and interesting applications.  I am always eager to expand my knowledge and open to new opportunities in the tech industry where I can bring my customer-first attitude and youthful passion to any team. </p>
+                        <p className="float-xs-left">I am currently living in San Jose, CA but open to relocation.  Find me on:</p>
+                        <Col size="xs-6" className="float-left">
+                            <a href="https://www.linkedin.com/in/rachelkroetch/" target="_blank" rel="noreferrer" className="social"><i className="fab fa-linkedin"></i></a>
+                            <a href="https://github.com/rekroetch" target="_blank" rel="noreferrer" className="social"><i className="fab fa-github-square"></i></a>
+                        </Col>
+                    </Col>
+                </Row> 
+            </Jumbotron>
+        </Container>
+    );
+  }
+  
+  export default About;
