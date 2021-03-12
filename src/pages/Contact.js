478806ca983f62badb1a61dc803f872cb8b4458f
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-12 11:10:15 -0800

    form added, limited functionality

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,46 @@
+import React, { Component } from "react";
 import Container from '../components/Container'
 import Jumbotron from '../components/Jumbotron'
+// import Input from '../components/Input'
+// import FormBtn from '../components/FormBtn'
+import Form from '../components/Form'
 
-function Contact() {
-    return (
-        <Container>
-            <Jumbotron>
-                <h1 className="display-4">Contact</h1>
-                <hr className="my-4"></hr>
-            </Jumbotron>
-        </Container>
-    );
-  }
+class Contact extends Component {
+    state = {
+        name: "",
+        email: "",
+        message: "",
+    };
+
+    handleInputChange = event => {
+        this.setState({ 
+            name: event.target.value,
+            email: event.target.value,
+            message: event.target.value
+        });
+        console.log(this.state)
+    };
+    
+    handleFormSubmit = () => {
+        // This is a lie. I do not plan on keeping this page in my real portfolio. Only for this assignment.
+        alert("Message sent!")
+    };
+
+
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <h1 className="display-4">Contact</h1>
+                    <hr className="my-4"></hr>
+                    <Form
+                        handleFormSubmit={this.handleFormSubmit}
+                        handleInputChange={this.handleInputChange}
+                    />
+                </Jumbotron>
+            </Container>
+        );
+    }
+}
   
-  export default Contact;
+export default Contact;
