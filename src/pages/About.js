30766c9314c9023352c56706c34038af3460107f
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-12 13:27:18 -0800

    linkedin and github icons changed to text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import './about.css'
+import './pages.css'
 import profPic from '../assets/profilePic.jpeg'
 import Container from '../components/Container'
 import Jumbotron from '../components/Jumbotron'
@@ -13,20 +13,21 @@ function About() {
                 <hr className="my-4"></hr>
                 <Row>
                     <Col size="md-12">
-                        <h4 className="contactInfo">Rachel Kroetch | (408)613-9497 | rkroetch8@gmail.com</h4>
+                        <h3 className="contactInfo">Rachel Kroetch | (408)613-9497 | rkroetch8@gmail.com</h3>
                     </Col>
                 </Row>   
                 <Row>
                     <Col size="xs-6" className="float-left">
                         <img src={profPic} className="cardImg float-left" alt="teal shirt"></img>
                         <p className="float-xs-left">As a full-stack web developer with a Bachelor of Science in Neurobiology from the University of California, Davis, I use my passion for problem solving and excellent time management skills to tackle every task presented to me.  These skills, along with a high level of teamwork and communication, were especially demonstrated during my undergraduate years at UC Davis, working on campus as a kitchen employee before being promoted to Supervisor in less than a year.  Later, while earning a certificate in full stack development from a UC Davis Extension, I refined my technical skills with JavaScript, using it to create dynamic and interesting applications.  I am always eager to expand my knowledge and open to new opportunities in the tech industry where I can bring my customer-first attitude and youthful passion to any team. </p>
-                        <p className="float-xs-left">I am currently living in San Jose, CA but open to relocation.  Find me on:</p>
-                        <Col size="xs-6" className="float-left">
-                            <a href="https://www.linkedin.com/in/rachelkroetch/" target="_blank" rel="noreferrer" className="social"><i className="fab fa-linkedin"></i></a>
-                            <a href="https://github.com/rekroetch" target="_blank" rel="noreferrer" className="social"><i className="fab fa-github-square"></i></a>
-                        </Col>
+                        <p className="float-xs-left">I am currently living in San Jose, CA but open to relocation. Find me on:</p>
                     </Col>
                 </Row> 
+                <Row>
+                    <Col size="md-12">
+                        <h4 className="contactInfo">linkedin.com/in/rachelkroetch | github.com/rekroetch</h4>
+                    </Col>
+                </Row>   
             </Jumbotron>
         </Container>
     );
