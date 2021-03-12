b8731379270511aca3d2b6746e7ce9e1b27ac95d
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-03-12 11:07:54 -0800

    fixed background color

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 function Jumbotron(props) {
-    return <main className="jumbotron">{props.children}</main>;
+    return <main className="jumbotron" style={{background:"#c1d3c6"}}>{props.children}</main>;
   }
   
   export default Jumbotron;
